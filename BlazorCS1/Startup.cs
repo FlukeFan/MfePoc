83d8220fce4a55b051474baad4afb1774b25aa46
Richard Brown <fluke_fan@hotmail.com>
2021-04-09 14:42:43 +0100

    Added BlazorCS1 project.

---
+++ BlazorCS1/Startup.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace MfePoc.BlazorCS1
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment hostEnvironment)
+        {
+            HostEnvironment = hostEnvironment;
+        }
+
+        public IWebHostEnvironment HostEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.Configure<MvcRazorRuntimeCompilationOptions>(opt =>
+            {
+                var libPath = Path.Combine(HostEnvironment.ContentRootPath, "..", "Shared");
+                var libFullPath = Path.GetFullPath(libPath);
+                opt.FileProviders.Add(new PhysicalFileProvider(libFullPath));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+            });
+        }
+    }
+}

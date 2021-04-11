b7bad8056dbaca8388d09239dc36ef75fa190e9e
Richard Brown <fluke_fan@hotmail.com>
2021-04-11 13:21:21 +0100

    Added sftp tool.

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -3,7 +3,7 @@
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 @SET PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;%PATH%
 type readme.md
-@doskey d=dotnet msbuild build.proj /t:Deploy
+@doskey d=dotnet msbuild build.proj /t:Deploy $*
 @echo.
 @echo Aliases:
 @echo.

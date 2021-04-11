c0ebf5f30103de19b82f222c7f860ca5fca05f1d
Richard Brown <fluke_fan@hotmail.com>
2021-04-11 16:15:05 +0100

    Added blank exception handler for now.

--- Home/Startup.cs
+++ Home/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
@@ -50,7 +51,7 @@ namespace MfePoc.Home
             }
             else
             {
-                app.UseExceptionHandler();
+                app.UseExceptionHandler(new ExceptionHandlerOptions { ExceptionHandler = ctx => Task.CompletedTask });
             }
 
             app.UseStaticFiles();

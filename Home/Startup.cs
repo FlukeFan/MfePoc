7011377fbeb844368ea757dcbab43f40c614fbf4
Richard Brown <fluke_fan@hotmail.com>
2021-04-11 14:39:01 +0100

    Added ZipDeploy to make deployment simple.

--- Home/Startup.cs
+++ Home/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using ZipDeploy;
 
 namespace MfePoc.Home
 {
@@ -23,6 +24,8 @@ namespace MfePoc.Home
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddZipDeploy();
+
             services
                 .AddControllersWithViews()
                 .AddRazorRuntimeCompilation();

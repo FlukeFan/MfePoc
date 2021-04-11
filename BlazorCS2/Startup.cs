16337a1545a9b57b7b39b2e19d5bf8da054ea605
Richard Brown <fluke_fan@hotmail.com>
2021-04-11 10:47:35 +0100

    Added Blazor client-side project 2.

---
+++ BlazorCS2/Startup.cs
@@ -0,0 +1,51 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace MfePoc.BlazorCS2
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment hostEnvironment)
+        {
+            HostEnvironment = hostEnvironment;
+        }
+
+        public IWebHostEnvironment HostEnvironment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddRazorPages()
+                .AddRazorRuntimeCompilation();
+
+            services.Configure<MvcRazorRuntimeCompilationOptions>(opt =>
+            {
+                var libPath = Path.Combine(HostEnvironment.ContentRootPath, "..", "Shared");
+                var libFullPath = Path.GetFullPath(libPath);
+                opt.FileProviders.Add(new PhysicalFileProvider(libFullPath));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}

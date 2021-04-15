babe7125d0b32f640272cf1d520efa1aa5851d4d
Richard Brown <fluke_fan@hotmail.com>
2021-04-15 17:02:20 +0100

    Fleshed out colour service.

---
+++ BlazorSS1/Stock.cs
@@ -0,0 +1,24 @@
+﻿namespace MfePoc.BlazorSS1
+{
+    public static class Stock
+    {
+        public static int Red { get; private set; }
+        public static int Green { get; private set; }
+        public static int Blue { get; private set; }
+
+        public static void GenerateRed()
+        {
+            Red++;
+        }
+
+        public static void GenerateGreen()
+        {
+            Green++;
+        }
+
+        public static void GenerateBlue()
+        {
+            Blue++;
+        }
+    }
+}

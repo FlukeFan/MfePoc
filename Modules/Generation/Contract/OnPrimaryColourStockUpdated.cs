119cb7a19060ec64c0d0a957729e5ba617c996e2
Richard Brown <fluke_fan@hotmail.com>
2021-04-21 17:08:45 +0100

    Added Generation Contract.

---
+++ Modules/Generation/Contract/OnPrimaryColourStockUpdated.cs
@@ -0,0 +1,9 @@
+﻿namespace MfePoc.Generation.Contract
+{
+    public class OnPrimaryColourStockUpdated
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}

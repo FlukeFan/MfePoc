9c89d13131bc239d2fe4bfd32c5e71ceaf172073
Richard Brown <fluke_fan@hotmail.com>
2021-04-26 16:22:48 +0100

    Added generation controls to dashboard (in i-frames).

---
+++ Modules/Generation/Contract/GenerationControls.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace MfePoc.Generation.Contract
+{
+    public static class GenerationControls
+    {
+        public static HtmlString StockLevels()
+        {
+            return FramedControl("StockLevels", 150, 80);
+        }
+
+        public static HtmlString RedGenerator()
+        {
+            return FramedControl("RedButton", 150, 40);
+        }
+
+        public static HtmlString GreenGenerator()
+        {
+            return FramedControl("GreenButton", 150, 40);
+        }
+
+        public static HtmlString BlueGenerator()
+        {
+            return FramedControl("BlueButton", 150, 40);
+        }
+
+        private static HtmlString FramedControl(string name, int width, int height)
+        {
+            return new HtmlString($"<iframe src=\"/Generation/Control?control={name}\" frameborder=\"0\" style=\"width:{width}px; height:{height}px\"></iframe>");
+        }
+    }
+}

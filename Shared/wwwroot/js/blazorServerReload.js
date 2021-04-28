ec7ccadcfda04217bddfab53335908a68a07f412
Richard Brown <fluke_fan@hotmail.com>
2021-04-28 10:22:10 +0100

    Ensured i-frame controls also reload.

---
+++ Shared/wwwroot/js/blazorServerReload.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var reload = function () {
+        $.ajax({
+            url: document.location.href,
+            error: function () { window.setTimeout(reload, 300); },
+            success: function () { document.location.reload(); }
+        });
+    }
+
+    Blazor.defaultReconnectionHandler._reconnectCallback = reload;
+}());

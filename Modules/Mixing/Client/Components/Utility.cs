368c0cf7bec47f1e33707f942909aaec34b3f055
Richard Brown <fluke_fan@hotmail.com>
2021-04-29 16:03:15 +0100

    Added primary colours to mix screen.

---
+++ Modules/Mixing/Client/Components/Utility.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Html;
+
+namespace MfePoc.Mixing.Client.Components
+{
+    public static class Utility
+    {
+        public static MarkupString Raw(this HtmlString value)
+        {
+            return new MarkupString(value.Value);
+        }
+    }
+}

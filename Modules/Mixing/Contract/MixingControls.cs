55b61a359b5d587bdce9082a2959bb6ed4bb5d5a
Richard Brown <fluke_fan@hotmail.com>
2021-04-30 11:12:08 +0100

    Added custom control in i-frame to Mixing.

---
+++ Modules/Mixing/Contract/MixingControls.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace MfePoc.Mixing.Contract
+{
+    public static class MixingControls
+    {
+        public static HtmlString StockLevels()
+        {
+            return FramedControl("StockLevels", 156, 52);
+        }
+
+        public static HtmlString ColourGenerators()
+        {
+            return FramedControl("Mixers", 150, 120);
+        }
+
+        private static HtmlString FramedControl(string name, int width, int height)
+        {
+            return new HtmlString($"<iframe src=\"/Mixing/Control?control={name}\" frameborder=\"0\" style=\"width:{width}px; height:{height}px\"></iframe>");
+        }
+    }
+}

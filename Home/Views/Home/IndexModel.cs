bd231652b8d98daf7e63b53d5cdbb535f41d6c4f
Richard Brown <fluke_fan@hotmail.com>
2021-05-01 14:46:58 +0100

    Removed hard-coded constants for warming up each module.

---
+++ Home/Views/Home/IndexModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MfePoc.Home.Views.Home
+{
+    public static class IndexModel
+    {
+        public static IEnumerable<string> WarmupUrls(IWebHostEnvironment host)
+        {
+            var warmupUrls = new List<string>();
+            var rootPath = Path.Combine(host.ContentRootPath, "..");
+
+            if (host.IsDevelopment())
+                while (!File.Exists(Path.Combine(rootPath, "MfePoc.sln")))
+                    rootPath = Path.Combine(rootPath, "..");
+
+            var moduleFiles = Directory.GetFiles(rootPath, "module.xml", SearchOption.AllDirectories);
+
+            foreach (var moduleFile in moduleFiles)
+            {
+                if (moduleFile.Contains($"{Path.DirectorySeparatorChar}bin"))
+                    continue; // skip any build artefacts
+
+                var doc = new XmlDocument();
+                doc.Load(moduleFile);
+
+                var warmupElement = (XmlElement)doc.SelectSingleNode("//Warmup");
+                warmupUrls.Add(warmupElement.GetAttribute("Url"));
+            }
+
+            return warmupUrls;
+        }
+    }
+}

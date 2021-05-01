718f246d9244ea0a291d6bdd9bd9f177e2f42f18
Richard Brown <fluke_fan@hotmail.com>
2021-05-01 12:19:19 +0100

    Added global notify function so controls in an iframe can show notifications.

---
+++ Shared/wwwroot/js/mfeNotify.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    window.mfeNotify = function (message) {
+        var inIframe = window.self !== window.top;
+
+        if (inIframe) {
+            window.top.$(window.top).trigger('notify', { message: message });
+        } else {
+            internalNotify(message);
+        }
+    }
+
+    $(window).on('notify', function (e, d) {
+        internalNotify('posted: ' + d.message);
+    });
+
+    function internalNotify(message) {
+        console.log(message);
+    }
+
+}());

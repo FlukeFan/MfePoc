db992fb62d45002f56ea2597621f09baf81daae9
Richard Brown <fluke_fan@hotmail.com>
2021-05-04 16:50:35 +0100

    Added report to dashboard.

---
+++ Modules/Reporting/Contract/ReportingControls.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace MfePoc.Reporting.Contract
+{
+    public static class ReportingControls
+    {
+        public static HtmlString Report()
+        {
+            return new HtmlString($"<iframe src=\"/Reporting/Control?control=SalesReport\" frameborder=\"0\" style=\"width:100%;\"></iframe>");
+        }
+    }
+}

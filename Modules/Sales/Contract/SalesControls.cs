55c34c5f14489d8f9da5e5649de3d1ae651b5956
Richard Brown <fluke_fan@hotmail.com>
2021-05-04 10:36:44 +0100

    Added selling controls to Dashboard.

---
+++ Modules/Sales/Contract/SalesControls.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace MfePoc.Sales.Contract
+{
+    public static class SalesControls
+    {
+        public static HtmlString SellControls()
+        {
+            return new HtmlString($"<iframe src=\"/Sales/Control?control=SellControls\" frameborder=\"0\" style=\"width:100%; height:340px\"></iframe>");
+        }
+    }
+}
